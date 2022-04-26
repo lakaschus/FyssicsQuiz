da78226a541b4989b243af07844ceb83d39f6df3
Phillip Lakaschus <Philliplakaschus@gmail.com>
2022-04-26 23:36:30 +0200

    List all question categories in playground

--- src/playground/testing/questions/index.vue
+++ src/playground/testing/questions/index.vue
@@ -1,4 +1,29 @@
 <template lang="">
+  <n-tabs size="large" justify-content="space-evenly" v-if="allQuestions">
+    <n-tab-pane
+      :name="key"
+      :tab="key"
+      v-for="key in Object.keys(this.allQuestions)"
+    >
+        <n-tabs size="large" justify-content="space-evenly" v-if="allQuestions">
+    <n-tab-pane
+      :name="subKey"
+      :tab="subKey"
+      v-for="subKey in Object.keys(this.allQuestions[key])"
+    >
+        <n-tabs size="large" justify-content="space-evenly" v-if="allQuestions">
+    <n-tab-pane
+      :name="subSubKey"
+      :tab="subSubKey"
+      v-for="subSubKey in Object.keys(this.allQuestions[key][subKey])"
+    >
+      {{ key }}
+    </n-tab-pane>
+  </n-tabs>
+    </n-tab-pane>
+  </n-tabs>
+    </n-tab-pane>
+  </n-tabs>
   <DynamicComponent path="./../../questions/__tests__/question1/question.vue" />
   <DynamicComponent path="./../../questions/NewtonsLawsOfMotion/question.vue" />
   <DynamicComponent path="./../../questions/NewtonsLawsOfMotion/answer.vue" />
@@ -6,10 +31,14 @@
 <script>
 import DynamicComponent from "@/components/Dynamics/DynamicComponent.vue"
 import { getAllQuestions } from "@/utils/questionHelpers/compileQuestions.js"
+import { NCard, NTabs, NTabPane } from "naive-ui"
 
 export default {
   components: {
     DynamicComponent,
+    NCard,
+    NTabs,
+    NTabPane,
   },
   data() {
     return {
@@ -17,9 +46,12 @@ export default {
     }
   },
   mounted() {
-    getAllQuestions().then((data) => {
+    getAllQuestions("../..").then((data) => {
       this.allQuestions = data
-      console.log("🚀 ~ file: index.vue ~ line 22 ~ getAllQuestions ~ this.allQuestions", this.allQuestions)
+      console.log(
+        "🚀 ~ file: index.vue ~ line 22 ~ getAllQuestions ~ this.allQuestions",
+        Object.keys(this.allQuestions)
+      )
     })
   },
 }
