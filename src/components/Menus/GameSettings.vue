f4308779bcb4ffea982c39574e6c602d16b7c3bb
Phillip Lakaschus <Philliplakaschus@gmail.com>
2022-05-06 00:22:46 +0200

    Add RangeSlider component for selecting difficulty

--- src/components/Menus/GameSettings.vue
+++ src/components/Menus/GameSettings.vue
@@ -1,32 +1,11 @@
 <template>
-  <FormKit
-    v-model="value"
-    type="checkbox"
-    label="Trim extras"
-    :options="{
-      650: 'Leather seats ($650)',
-      1200: 'Sweet rims ($1,200)',
-      13250: 'Gold leaf ($13,250)',
-      500: 'Massaging seats ($500)'
-    }"
-    help="Configure your car’s trim options"
-  />
-  <FormKit
-  type="number"
-  help="Enter the number of rounds you'd like to play"
-  label="Rounds"
-  name="Rounds"
-  value="10"
-  step="1"
-/>
-  <pre wrap>Extra cost: ${{ sum }}</pre>
+  <difficulty-slider @sliderRange="print"></difficulty-slider>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
-const value = ref([])
-const sum = computed(() => {
-  return new Intl.NumberFormat('en-US')
-    .format(value.value.reduce((sum, price) => Number(price) + sum, 0))
-})
+import DifficultySlider from './components/DifficultySlider.vue'
+
+function print(arg) {
+  console.log("print: ", arg)
+}
 </script>
---
+++ src/components/Menus/components/DifficultySlider.vue
@@ -0,0 +1,40 @@
+<template>
+  <n-grid cols="24" item-responsive responsive="screen">
+    <n-gi content-style="-webkit-transform: rotate(-90deg);" style="-webkit-transform: rotate(-90deg); width: 250px;" span="1">
+     Difficulty Range
+    </n-gi>
+    <n-gi content-style="padding: 12px;" span="1">
+      <div style="height: 250px; justify-content: center">
+        <n-slider
+          v-model:value="sliderData.value"
+          :marks="sliderData.marks"
+          :min="sliderData.min"
+          :max="sliderData.max"
+          vertical
+          range
+        />
+      </div>
+    </n-gi>
+  </n-grid>
+</template>
+
+<script setup>
+import { reactive, watch } from "vue"
+import { NGrid, NSlider, NLayoutContent, NGridItem, NGi } from "naive-ui"
+
+const sliderData = reactive({
+  value: [0, 9],
+  min: 0,
+  max: 9,
+  marks: { ...Array.from({ length: 10 }, (_, i) => i + 1) },
+})
+
+const emit = defineEmits(["sliderRange"])
+watch(
+  sliderData,
+  (data) => {
+    emit("sliderRange", data.value)
+  },
+  { deep: true }
+)
+</script>
