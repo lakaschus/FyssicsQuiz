e6ec83da63ad69ad8af9dd6465f081a77259e84b
Phillip Lakaschus <Philliplakaschus@gmail.com>
2022-05-10 00:14:47 +0200

    Extract categories object from checked keys array + test case

--- src/utils/questionHelpers/__tests__/getRandomQuestions.spec.js
+++ src/utils/questionHelpers/__tests__/getRandomQuestions.spec.js
@@ -1,11 +1,12 @@
-import { expect, it, describe } from 'vitest'
-import { getRandomQuestion } from '../getRandomQuestions'
+import { expect, it, describe, beforeAll } from 'vitest'
+import { getAllQuestions } from '../compileQuestions'
+import { extractCategories, getRandomQuestion } from '../getRandomQuestions'
 
 const categories = [
   'Quantum Chromodynamics (parent: High-Energy Physics (parent: Theory))',
+  'Classical Mechanics (parent: Theory)',
   'Standard Model (parent: High-Energy Physics (parent: Theory))',
   'Quantum Electrodynamics (parent: High-Energy Physics (parent: Theory))',
-  'Classical Mechanics (parent: Theory)',
   'Lagrangian Mechanics (parent: Classical Mechanics (parent: Theory))',
   'Hamiltonian Mechanics (parent: Classical Mechanics (parent: Theory))',
   'Kinematics (parent: Classical Mechanics (parent: Theory))',
@@ -13,6 +14,22 @@ const categories = [
 ]
 
 describe('Get collection of random questions', function () {
+  beforeAll(async () => {
+    // creates a fresh pinia and make it active so it's automatically picked
+    // up by any useStore() call without having to pass it to it:
+    // `useStore(pinia)`
+    const allQuestions = await getAllQuestions()
+  })
+  it('Convert checked key to category object', function () {
+    const categoriesObject1 = extractCategories(categories[0])
+    const categoriesObject2 = extractCategories(categories[1])
+    expect(categoriesObject1.category).toBe('Theory')
+    expect(categoriesObject1.subCategory).toBe('High-Energy Physics')
+    expect(categoriesObject1.specialCategory).toBe('Quantum Chromodynamics')
+    expect(categoriesObject2.category).toBe('Theory')
+    expect(categoriesObject2.subCategory).toBe('Classical Mechanics')
+    expect(categoriesObject2.specialCategory).toBe('')
+  })
   it('Get a single question', function () {
     const questionPath = getRandomQuestion({ difficulty: [2, 5], categories })
     expect(questionPath).toBe()
--- src/utils/questionHelpers/getRandomQuestions.js
+++ src/utils/questionHelpers/getRandomQuestions.js
@@ -1,5 +1,33 @@
 import { useQuestionsStore } from '@/stores/questions'
 
+export function extractCategories (checkedKey) {
+  console.log(
+    '🚀 ~ file: getRandomQuestions.js ~ line 4 ~ extractCategories ~ checkedKey',
+    checkedKey
+  )
+  /**
+   * Extract categories from checked category keys (See SelectCategories.vue).
+   * E.g.: 'Quantum Chromodynamics (parent: High-Energy Physics (parent: Theory))'
+   * -> {
+   * category: 'Theory',
+   * subCategory: 'High-Energy Physics',
+   * specialCategory: 'Quantum Chromodynamics
+   * }
+   */
+  const categoriesArray = checkedKey
+    .split('(')
+    .join('')
+    .split(')')
+    .join('')
+    .split('parent: ')
+  const categoryObject = {
+    category: categoriesArray.slice(-1)[0].trim(),
+    subCategory: categoriesArray.slice(-2, -1).length ? categoriesArray.slice(-2, -1)[0].trim() : '',
+    specialCategory: categoriesArray.slice(-3, -2).length ? categoriesArray.slice(-3, -2)[0].trim() : ''
+  }
+  return categoryObject
+}
+
 export function getRandomQuestion (settings) {
   /**
    * Returns a random question respecting the settings constraints
