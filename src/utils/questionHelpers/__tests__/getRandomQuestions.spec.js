af27d21ec0d451312760b2e27bc91d3fb8132834
Phillip Lakaschus <Philliplakaschus@gmail.com>
2022-05-11 23:22:26 +0200

    Add function to get random question given category and difficulty settings

--- src/utils/questionHelpers/__tests__/getRandomQuestions.spec.js
+++ src/utils/questionHelpers/__tests__/getRandomQuestions.spec.js
@@ -1,3 +1,5 @@
+import { useQuestionsStore } from '@/stores/questions'
+import { setActivePinia, createPinia } from 'pinia'
 import { expect, it, describe, beforeAll } from 'vitest'
 import { getAllQuestions } from '../compileQuestions'
 import { extractCategories, getRandomQuestion } from '../getRandomQuestions'
@@ -5,6 +7,7 @@ import { extractCategories, getRandomQuestion } from '../getRandomQuestions'
 const categories = [
   'Quantum Chromodynamics (parent: High-Energy Physics (parent: Theory))',
   'Classical Mechanics (parent: Theory)',
+  'Linear Algebra (parent: Mathematics)',
   'Standard Model (parent: High-Energy Physics (parent: Theory))',
   'Quantum Electrodynamics (parent: High-Energy Physics (parent: Theory))',
   'Lagrangian Mechanics (parent: Classical Mechanics (parent: Theory))',
@@ -13,25 +16,45 @@ const categories = [
   'Kinetics (parent: Classical Mechanics (parent: Theory))'
 ]
 
-describe('Get collection of random questions', function () {
+describe('Get collection of random questions', async function () {
   beforeAll(async () => {
     // creates a fresh pinia and make it active so it's automatically picked
     // up by any useStore() call without having to pass it to it:
-    // `useStore(pinia)`
-    const allQuestions = await getAllQuestions()
+    // `useStore(pinia)`+
+    setActivePinia(createPinia())
+    const questions = useQuestionsStore()
+    const importQuestions = await getAllQuestions()
+    questions.$patch({ questions: importQuestions })
   })
   it('Convert checked key to category object', function () {
     const categoriesObject1 = extractCategories(categories[0])
     const categoriesObject2 = extractCategories(categories[1])
+    const categoriesObject3 = extractCategories(categories[2])
     expect(categoriesObject1.category).toBe('Theory')
     expect(categoriesObject1.subCategory).toBe('High-Energy Physics')
     expect(categoriesObject1.specialCategory).toBe('Quantum Chromodynamics')
     expect(categoriesObject2.category).toBe('Theory')
     expect(categoriesObject2.subCategory).toBe('Classical Mechanics')
     expect(categoriesObject2.specialCategory).toBe('')
+    expect(categoriesObject3.category).toBe('Mathematics')
+    expect(categoriesObject3.subCategory).toBe('Linear Algebra')
+    expect(categoriesObject3.specialCategory).toBe('')
   })
-  it('Get a single question', function () {
-    const questionPath = getRandomQuestion({ difficulty: [2, 5], categories })
-    expect(questionPath).toBe()
+  it('Get a single question', async function () {
+    for (let i = 0; i < 10; i++) {
+      const questionPath = await getRandomQuestion({
+        difficulty: [1, 9],
+        categories: [
+          'Quantum Chromodynamics (parent: High-Energy Physics (parent: Theory))',
+          'Classical Mechanics (parent: Theory)',
+          'Linear Algebra (parent: Mathematics)'
+        ]
+      })
+      expect(questionPath).toContain('@/questions/')
+      console.log(
+        '🚀 ~ file: getRandomQuestions.spec.js ~ line 54 ~ questionPath',
+        questionPath
+      )
+    }
   })
 })
--- src/utils/questionHelpers/getRandomQuestions.js
+++ src/utils/questionHelpers/getRandomQuestions.js
@@ -1,10 +1,7 @@
 import { useQuestionsStore } from '@/stores/questions'
+import { getMeta } from './compileQuestions'
 
 export function extractCategories (checkedKey) {
-  console.log(
-    '🚀 ~ file: getRandomQuestions.js ~ line 4 ~ extractCategories ~ checkedKey',
-    checkedKey
-  )
   /**
    * Extract categories from checked category keys (See SelectCategories.vue).
    * E.g.: 'Quantum Chromodynamics (parent: High-Energy Physics (parent: Theory))'
@@ -22,13 +19,17 @@ export function extractCategories (checkedKey) {
     .split('parent: ')
   const categoryObject = {
     category: categoriesArray.slice(-1)[0].trim(),
-    subCategory: categoriesArray.slice(-2, -1).length ? categoriesArray.slice(-2, -1)[0].trim() : '',
-    specialCategory: categoriesArray.slice(-3, -2).length ? categoriesArray.slice(-3, -2)[0].trim() : ''
+    subCategory: categoriesArray.slice(-2, -1).length
+      ? categoriesArray.slice(-2, -1)[0].trim()
+      : '',
+    specialCategory: categoriesArray.slice(-3, -2).length
+      ? categoriesArray.slice(-3, -2)[0].trim()
+      : ''
   }
   return categoryObject
 }
 
-export function getRandomQuestion (settings) {
+export async function getRandomQuestion (settings) {
   /**
    * Returns a random question respecting the settings constraints
    * @param settings Object containg the settings, e.g. difficulty and
@@ -39,5 +40,53 @@ export function getRandomQuestion (settings) {
    * }
    */
   const allQuestions = useQuestionsStore().questions
-  const { difficulty, categories } = settings
+  const difficulty = settings.difficulty
+  const categories = settings.categories
+
+  let selectedQuestionPaths
+
+  for (let i = 0; i < 100; i++) {
+    const category = categories[Math.floor(Math.random() * categories.length)]
+    const categoryObject = extractCategories(category)
+
+    let maxLevel = -1
+    for (const key in categoryObject) {
+      if (categoryObject[key]) {
+        maxLevel++
+      }
+    }
+    const randomLevel = Math.floor(Math.random() * (maxLevel + 1))
+    if (randomLevel === 0) {
+      selectedQuestionPaths = allQuestions[categoryObject.category].questions
+    }
+    if (randomLevel === 1) {
+      selectedQuestionPaths =
+        allQuestions[categoryObject.category][categoryObject.subCategory]
+          .questions
+    }
+    if (randomLevel === 2) {
+      selectedQuestionPaths =
+        allQuestions[categoryObject.category][categoryObject.subCategory][
+          categoryObject.specialCategory
+        ].questions
+    }
+
+    if (selectedQuestionPaths.length === 0) {
+      continue
+    }
+
+    const selectedQuestion =
+      selectedQuestionPaths[
+        Math.floor(Math.random() * selectedQuestionPaths.length)
+      ]
+
+    const metaInfo = await getMeta(selectedQuestion + 'meta.json')
+    if (
+      metaInfo.difficulty > difficulty[0] &&
+      metaInfo.difficulty < difficulty[1]
+    ) {
+      return selectedQuestion
+    }
+  }
+  return 'No fitting question found after 100 tries!'
 }
