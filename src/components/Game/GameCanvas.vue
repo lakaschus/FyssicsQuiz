c6ac4406974b7847b223ee5c87ed90dd12da88e2
Phillip Lakaschus <Philliplakaschus@gmail.com>
2022-05-27 23:44:38 +0200

    Start building game canvas

---
+++ src/components/Game/GameCanvas.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        GameCanvas
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
--- src/playground/testing/components/index.vue
+++ src/playground/testing/components/index.vue
@@ -4,18 +4,23 @@
       <n-tab-pane name="GameSettings">
         <Game-Settings></Game-Settings>
       </n-tab-pane>
+      <n-tab-pane name="GameCanvas">
+        <Game-Canvas></Game-Canvas>
+      </n-tab-pane>
     </n-tabs>
   </n-card>
 </template>
 <script>
 import { NCard, NTabs, NTabPane } from "naive-ui"
 import GameSettings from "../../../components/Menus/GameSettings.vue"
+import GameCanvas from "../../../components/Game/GameCanvas.vue"
 
 export default {
   components: {
     NCard,
     NTabs,
     NTabPane,
+    GameCanvas,
     GameSettings,
   },
 }
